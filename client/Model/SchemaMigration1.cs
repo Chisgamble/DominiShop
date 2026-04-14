a0500959ece34f58b29216a5d4f6032853ebf237
Chis <chitrankhon@gmail.com>
2026-04-14 19:27:48 +0700

    Intall packages

---
+++ client/Model/SchemaMigration1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DominiShop.Model;
+
+public partial class SchemaMigration1
+{
+    public long Version { get; set; }
+
+    public DateTime? InsertedAt { get; set; }
+}
